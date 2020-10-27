263bf808c2c4276c63571ac95e44a625d3c56222
hup <hup@hzphfin.com>
2020-10-27 16:13:57 +0800

    init

---
+++ src/main/java/com/demo/helloworld/controller/HelloController.java
@@ -0,0 +1,32 @@
+package com.demo.helloworld.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @ClassName HelloController
+ * @Description
+ * @Author hup
+ * @Date 2020/10/26 11:31
+ * @Version 1.0
+ */
+
+@RestController
+@RequestMapping("hello")
+@Slf4j
+public class HelloController {
+
+    @GetMapping("")
+    public String hello() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String nowStr = simpleDateFormat.format(new Date());
+        log.info("request success,time : {}", nowStr);
+        return nowStr + ",Hello World! \n yeah";
+    }
+
+}
